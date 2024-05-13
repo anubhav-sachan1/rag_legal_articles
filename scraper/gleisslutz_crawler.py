7d3577adbb0a5346100f5a6344df6c5be3cbc478
Partho Sarthi <psarthi@nvidia.com>
2024-05-12 17:20:14 -0700

    Add gleisslutz

---
+++ scraper/gleisslutz_crawler.py
@@ -0,0 +1,72 @@
+import os
+import time
+from selenium.webdriver.common.by import By
+from datetime import datetime
+
+from scraper_base import Scraper, ScraperType
+
+class GleisSlutzScraper(Scraper):
+
+    def accept_cookies(self):
+        try:
+            cookie_button = self.driver.find_element(By.CSS_SELECTOR, '#ccm-widget button.ccm--save-settings')
+            # If button exists, click it
+            if cookie_button:
+                cookie_button.click()
+        except Exception as e:
+            print(f"Error: {e}")
+            print("Failed to accept cookies")
+
+    def fetch_data(self):
+        url = self.base_url
+        self.driver.get(url)
+        time.sleep(2)
+        self.accept_cookies()
+
+        elements = self.driver.find_elements(By.CSS_SELECTOR, "div.js-content-container-all article.node--type-knowhow")
+        last_date = datetime.today().date()
+        
+        for element in elements:
+            link = element.find_element(By.CSS_SELECTOR, 'a.teaser__more-link')
+            title = element.find_element(By.CSS_SELECTOR, "span.teaser__headline").text
+            doc_date = element.find_element(By.CSS_SELECTOR, "time").text
+            doc_date = self.convert_to_date_type(doc_date, '%d.%m.%Y').date()
+            if doc_date < last_date:
+                last_date = doc_date
+            doc_url = link.get_attribute('href')
+            if not doc_url.startswith('http'):
+                    doc_url = f"{self.get_scheme_and_domain()}{doc_url}"
+            self.data.append({
+                'title': title,
+                'date': doc_date,
+                'url': doc_url,
+                'type': ScraperType.HTML
+            })
+        return last_date
+
+
+
+    def download_html_and_save_csv(self):
+        csv_data = []
+        for entry in self.data:
+            try:
+                self.driver.get(entry['url'])
+                html_element = self.driver.find_element(By.CSS_SELECTOR, "div.paragraph__content-container")
+                html_content = html_element.get_attribute('outerHTML')
+                file_path = self.download_page_as_html(html_content, f"{entry['title']}.html")
+                csv_row = {'title': entry['title'], 'url': file_path, 'date': entry['date']}
+                csv_data.append(csv_row)
+            except Exception:
+                print(f"Failed to retrieve PDF URL for {entry['title']}")
+        
+        self.write_csv(csv_data)
+
+def main():
+    base_url = 'https://www.gleisslutz.com/en/news-events/know-how'
+    scraper = GleisSlutzScraper(base_url)
+    scraper.fetch_data()
+    scraper.download_html_and_save_csv()
+    scraper.close()
+
+if __name__ == "__main__":
+    main()
