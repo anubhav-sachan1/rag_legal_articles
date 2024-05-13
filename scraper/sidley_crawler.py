64d493987f0273b3c8f644f6d327cc0cbd17a05e
Partho Sarthi <psarthi@nvidia.com>
2024-05-12 21:07:30 -0700

    Fix dates

--- scraper/sidley_crawler.py
+++ scraper/sidley_crawler.py
@@ -17,6 +17,13 @@ class SidleyScraper(Scraper):
             link = element.find_element(By.CSS_SELECTOR, "a[data-bind*='NavigateLink.Url']")
             doc_date = element.find_element(By.CSS_SELECTOR, "span[data-bind='text: DisplayDate']").text
             doc_url = link.get_attribute('href')
+            date_formats = ['%B %d, %Y', '%B %Y']
+            for date_format in date_formats:
+                try:
+                    doc_date = self.convert_to_date_type(doc_date, date_format).date()
+                    break
+                except ValueError:
+                    pass
             self.data.append({
                 'title': title,
                 'date': doc_date,
@@ -28,14 +35,25 @@ class SidleyScraper(Scraper):
         for entry in self.data:
             try:
                 self.driver.get(entry['url'])
-                links = self.driver.find_elements(By.CSS_SELECTOR, "div.inner-wrapper a")
+                html_element = self.driver.find_element(By.CSS_SELECTOR, "div.inner-wrapper")
+                links = html_element.find_elements(By.CSS_SELECTOR, "a")
                 urls = [link.get_attribute('href') for link in links]
-                pdf_url = [url for url in urls if '.pdf' in url][0]
-                csv_row = {'title': entry['title'], 'url': pdf_url, 'date': entry['date']}
-                self.download_pdf_with_user_agent(pdf_url, f"{entry['title']}.pdf")
+                pdf_urls = [url for url in urls if '.pdf' in url]
+                if len(pdf_urls) > 0:
+                    pdf_url = pdf_urls[0]
+                    if not pdf_url.startswith('http'):
+                        pdf_url = f"{self.get_scheme_and_domain()}{pdf_url}"
+                    csv_row = {'title': entry['title'], 'url': pdf_url, 'date': entry['date']}
+                    self.download_pdf_with_user_agent(pdf_url, f"{entry['title']}.pdf")
+                else:
+                    summary_element = html_element.find_element(By.CSS_SELECTOR, "div.article-summary")
+                    html_content = summary_element.get_attribute('outerHTML')
+                    self.download_page_as_html(html_content, f"{entry['title']}.html")
+                    csv_row = {'title': entry['title'], 'url': entry['url'], 'date': entry['date']}
                 csv_data.append(csv_row)
-            except Exception:
+            except Exception as e:
                 print(f"Failed to retrieve PDF URL for {entry['title']}")
+                print(e)
         
         self.write_csv(csv_data)
 
