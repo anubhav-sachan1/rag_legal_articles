67b22a56c38a3cd37e502794847454d64626bfb8
Anubhav Sachan <anubhavsachan@Anubhavs-Laptop.local>
2024-05-16 16:47:12 -0700

    Sidley Crawler

--- scraper/sidley_crawler.py
+++ scraper/sidley_crawler.py
@@ -4,33 +4,59 @@ from selenium.webdriver.common.by import By
 
 from scraper_base import Scraper
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+
 class SidleyScraper(Scraper):
 
-    def fetch_data(self, page_number):
-        url = f'{self.base_url}&skip={page_number}'
-        self.driver.get(url)
-        time.sleep(2)
-        elements = self.driver.find_elements(By.CLASS_NAME, 'search-result')
-        
-        for element in elements:
-            title = element.find_element(By.CLASS_NAME, 'result-title').text
-            link = element.find_element(By.CSS_SELECTOR, "a[data-bind*='NavigateLink.Url']")
-            doc_date = element.find_element(By.CSS_SELECTOR, "span[data-bind='text: DisplayDate']").text
-            doc_url = link.get_attribute('href')
-            date_formats = ['%B %d, %Y', '%B %Y']
-            for date_format in date_formats:
-                try:
-                    doc_date = self.convert_to_date_type(doc_date, date_format).date()
-                    break
-                except ValueError:
-                    pass
-            self.data.append({
-                'title': title,
-                'date': doc_date,
-                'url': doc_url
-            })
+    def fetch_data(self):
+        self.driver.get(self.base_url)
+        time.sleep(2)  
 
-    def download_pdfs_and_prepare_csv(self):
+        last_date = datetime.today().date()
+        target_date = datetime.strptime('31.12.2021', '%d.%m.%Y').date()
+
+        while True:
+            elements = self.driver.find_elements(By.CLASS_NAME, 'search-result')
+            for element in elements:
+                title = element.find_element(By.CLASS_NAME, 'result-title').text
+                link = element.find_element(By.CSS_SELECTOR, "a[data-bind*='NavigateLink.Url']")
+                doc_date = element.find_element(By.CSS_SELECTOR, "span[data-bind='text: DisplayDate']").text
+                doc_url = link.get_attribute('href')
+                date_formats = ['%B %d, %Y', '%B %Y']
+
+                for date_format in date_formats:
+                    try:
+                        parsed_date = datetime.strptime(doc_date, date_format).date()
+                        if parsed_date > target_date:
+                            self.data.append({
+                                'title': title,
+                                'date': parsed_date,
+                                'url': doc_url
+                            })
+                        break
+                    except ValueError:
+                        continue
+
+                if parsed_date < last_date:
+                    last_date = parsed_date
+
+            if last_date <= target_date:
+                break
+
+            try:
+                load_more_button = WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-read-more.js-rte-trigger")))  
+                load_more_button.click()
+                time.sleep(2)  
+            except Exception as e:
+                print("Could not find the 'Load More' button or no more pages to load")
+                print(e)
+                break
+
+    def download_content_and_prepare_csv(self):
         csv_data = []
         for entry in self.data:
             try:
@@ -52,17 +78,16 @@ class SidleyScraper(Scraper):
                     csv_row = {'title': entry['title'], 'url': entry['url'], 'date': entry['date']}
                 csv_data.append(csv_row)
             except Exception as e:
-                print(f"Failed to retrieve PDF URL for {entry['title']}")
-                print(e)
+                print(f"Failed to retrieve content for {entry['title']}")
         
         self.write_csv(csv_data)
 
 def main():
     base_url = 'https://www.sidley.com/en/us/insights/?articletypes=9cbfe518-3bc0-4632-ae13-6ac9cee8eb31'
-    scraper = SidleyScraper(base_url)
-    page_number = 10  
-    scraper.fetch_data(page_number)
-    scraper.download_pdfs_and_prepare_csv()
+    scraper = SidleyScraper(base_url) 
+    scraper.fetch_data()
+    scraper.download_content_and_prepare_csv()
+    scraper.write_chunks_csv("Sidley", "div", "rich-text-content", True)
     scraper.close()
 
 if __name__ == "__main__":
